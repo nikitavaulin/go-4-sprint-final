516719c2731681400a6c744be3539d0124d437c7
nsvaulin <mr.vau2017@mail.ru>
2025-11-30 18:52:03 +0500

    Feat: implement functions in daysteps. Need: refactoring

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +17,52 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	splitData := strings.Split(data, ",")
+	if parsedCount := len(splitData); parsedCount != 2 {
+		return 0, 0, fmt.Errorf("wrong parsedCount, got: %d wanted: %d", parsedCount, 2)
+	}
+
+	stepsCount, err := strconv.Atoi(splitData[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("did not parse stepsCount: %w", err)
+	}
+
+	if stepsCount <= 0 {
+		return 0, 0, fmt.Errorf("stepsCount should be more than 0, got: %d wanted", stepsCount)
+	}
+
+	walkTime, err := time.ParseDuration(splitData[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("did not parse walkTime: %w", err)
+	}
+
+	return stepsCount, walkTime, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	stepsCount, walkTime, err := parsePackage(data)
+	if err != nil {
+		fmt.Printf("error: %w\n", err) // mb fix
+		return ""
+	}
+
+	distance := float64(stepsCount) * stepLength / float64(mInKm)
+
+	spentCalories, err := spentcalories.WalkingSpentCalories(
+		stepsCount,
+		weight,
+		height,
+		walkTime)
+	if err != nil {
+		fmt.Printf("error: %w\n", err) // mb fix
+		return ""
+	}
+
+	output := fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила: %.2f\nВы сожгли %.2f ккал.\n",
+		stepsCount,
+		distance,
+		spentCalories)
+
+	return output
 }
